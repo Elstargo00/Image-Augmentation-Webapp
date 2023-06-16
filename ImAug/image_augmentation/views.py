758c594baa8b7a9fc4a7473a05ee5f93b0f2fcc9
Elstargo00 <a.Thaprasop.J@icloud.com>
2023-06-16 12:52:49 +0700

    update the latest version to github

--- ImAug/image_augmentation/views.py
+++ ImAug/image_augmentation/views.py
@@ -83,7 +83,7 @@ def process_augmentation(request):
 
 
         # _____ _____ setup augmented outputs _____ _____
-        fn_suffix = "v1"
+        fn_suffix = "vtest"
         outs = Path(f"{settings.MEDIA_ROOT}/augmented_{fn_suffix}")
         image_outs = outs / "images"
         label_outs = outs / "labels"
@@ -91,32 +91,94 @@ def process_augmentation(request):
         label_outs.mkdir(parents=True, exist_ok=True)
         # ____ ____ ____ ____ ____ ____ ____ ____ ____
 
+        print(request.POST.get("TransformOption"))
 
-        transformat = TransFormat(outs)
+        # There are two options for applying augmentation
 
-        for each in transforming_list:
-            transformat.append_format(each)
-        
-        transform = transformat.compose(format="yolo", min_vis=0.7)
+        transform_option = request.POST.get("TransformOption")
 
-        dataset = TransPack(dataset_dir=f"{settings.MEDIA_ROOT}/datasets", transform=transform)
+        # 1st: 1 augment / 1 sample
+        if transform_option == "oneTrans":
+            for each in transforming_list:
+                # resetting factor
+                transformat = TransFormat(outs)
+                filename_extension = text_to_hex(
+                    extract_transforming_name(each["format_type"])
+                )
+                #
+                transformat.append_format(each)
+                transform = transformat.compose(format="yolo", min_vis=0.7)
 
-        for image_filename, image, label_filename, bboxes in dataset:
+                dataset = TransPack(
+                    dataset_dir = f"{settings.MEDIA_ROOT}/datasets",
+                    transform = transform
+                )
+                
+                for image_filename, image, label_filename, bboxes in dataset:
+
+                    label_filename = f"{label_filename[:-4]}_{fn_suffix}_{filename_extension}.txt"
+                    saved_label = label_outs / label_filename
+
+                    with open(saved_label, 'a') as label_file:
+                        for bbox in bboxes:
+                            label_file.write(f"{str(bbox[-1])} {str(bbox[0])} {str(bbox[1])} {str(bbox[2])} {str(bbox[3])}\n")
+
+                    image_filename = f"{image_filename[:-4]}_{fn_suffix}_{filename_extension}.png"
+                    saved_image = image_outs / image_filename
+                    image = Image.fromarray(image)
+                    image.save(saved_image)
 
 
-            label_filename, f"{label_filename[:-4]}_{fn_suffix}.txt"
-            saved_label = label_outs / label_filename
-            
-            with open(saved_label, 'a') as label_file:
-                for bbox in bboxes:
-                    label_file.write(f"{str(bbox[-1])} {str(bbox[0])} {str(bbox[1])} {str(bbox[2])} {str(bbox[3])}\n")
+        # 2nd: all augments / 1 sample
+        if transform_option == "AllTrans":
+            transformat = TransFormat(outs)
+            filename_extension = []
+            for each in transforming_list:
+                filename_extension.append(extract_transforming_name(each["format_type"]))
+                transformat.append_format(each)
 
-            image_filename = f"{image_filename[:-4]}_{fn_suffix}.png"
-            saved_image = image_outs / image_filename
-            image = Image.fromarray(image)
-            image.save(saved_image)
+            filename_extension = text_to_hex(", ".join(filename_extension))
+
+            print(filename_extension)
+
+            transform = transformat.compose(format="yolo", min_vis=0.7)
+
+            dataset = TransPack(
+                dataset_dir = f"{settings.MEDIA_ROOT}/datasets",
+                transform = transform
+            )
+
+            for image_filename, image, label_filename, bboxes in dataset:
+                label_filename = f"{label_filename[:-4]}_{fn_suffix}_{filename_extension}.txt"
+                saved_label = label_outs / label_filename
+
+                with open(saved_label, 'a') as label_file:
+                    for bbox in bboxes:
+                        label_file.write(f"{str(bbox[-1])} {str(bbox[0])} {str(bbox[1])} {str(bbox[2])} {str(bbox[3])}\n")
+
+                image_filename = f"{image_filename[:-4]}_{fn_suffix}_{filename_extension}.png"
+                saved_image = image_outs / image_filename
+                image = Image.fromarray(image)
+                image.save(saved_image)
 
 
         return HttpResponse("Finish Augmentation!")
     else:
         return HttpResponseRedirect('index')
+    
+
+
+
+# helper function
+def text_to_hex(text):
+    # encode the text to bytes, then convert to hexadecimal
+    hex_output = text.encode("utf-8").hex()
+    return hex_output
+
+
+def extract_transforming_name(obj):
+    # Get the full class name
+    full_name = str(obj)
+    # Extract the class name
+    class_name = full_name.split('.')[-1].replace("'>", '')
+    return class_name
