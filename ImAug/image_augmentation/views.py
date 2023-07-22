3b454a0b4ad1b3fb15a327bfa047fbff52132ad0
Elstargo00 <a.Thaprasop.J@icloud.com>
2023-07-22 09:57:19 +0700

    output naming

--- ImAug/image_augmentation/views.py
+++ ImAug/image_augmentation/views.py
@@ -20,12 +20,11 @@ def process_augmentation(request):
         
         if bool(request.POST.get("affine")):
             translate_percent = float(request.POST.get("affine_translate_percent")) # field check
-            p = request.POST.get("affine_p")
+            p = float(request.POST.get("affine_p"))
             rotate = float(request.POST.get("affine_rotate"))
             shear = ast.literal_eval(request.POST.get("affine_shear"))
             if not p:
                 p = 0.5
-            p = float(p)
             transforming_list.append({
                 "format_type": A.Affine,
                 "params": {
@@ -40,10 +39,9 @@ def process_augmentation(request):
         if bool(request.POST.get("random_crop")):
             width = int(float((request.POST.get("random_crop_width")))) # field check
             height = int(float(request.POST.get("random_crop_height"))) # field check
-            p = request.POST.get("random_crop_p")
+            p = float(request.POST.get("random_crop_p"))
             if not p:
                 p = 0.5
-            p = float(p) 
             transforming_list.append({
                 "format_type": A.RandomCrop,
                 "params": {"width": width, "height": height, "p": p}
@@ -53,10 +51,9 @@ def process_augmentation(request):
         if bool(request.POST.get("center_crop")):
             width = int(float(request.POST.get("center_crop_width"))) # field check
             height = int(float(request.POST.get("center_crop_height"))) # field check
-            p = request.POST.get("center_crop_p")
+            p = float(request.POST.get("center_crop_p"))
             if not p:
                 p = 0.5
-            p = float(p) 
             transforming_list.append({
                 "format_type": A.CenterCrop,
                 "params": {"width": width, "height": height, "p": p}
@@ -64,10 +61,9 @@ def process_augmentation(request):
 
 
         if bool(request.POST.get("horizontal_flip")):
-            p = request.POST.get("horizontal_flip_p")
+            p = float(request.POST.get("horizontal_flip_p"))
             if not p:
                 p = 0.5
-            p = float(p)
             transforming_list.append({
                 "format_type": A.HorizontalFlip,
                 "params": {"p": p}
@@ -78,7 +74,6 @@ def process_augmentation(request):
             p = float(request.POST.get("vertical_flip_p"))
             if not p:
                 p = 0.5
-            p = float(p)
             transforming_list.append({
                 "format_type": A.VerticalFlip,
                 "params": {'p': p}
@@ -88,7 +83,6 @@ def process_augmentation(request):
             p = float(request.POST.get("togray_p"))
             if not p:
                 p = 0.5
-            p = float(p)
             transforming_list.append({
                 "format_type": A.ToGray,
                 "params": {'p': p}
@@ -112,8 +106,13 @@ def process_augmentation(request):
 
 
         # _____ _____ setup augmented outputs _____ _____
-        fn_suffix = "vtest"
-        outs = Path(f"{settings.MEDIA_ROOT}/augmented_{fn_suffix}")
+        output_folder = request.POST.get("augmented_name")
+        tag_ver = request.POST.get("tag_version")
+        if not output_folder:
+            output_folder = "augmented"
+        if not tag_ver:
+            tag_ver = "V1"
+        outs = Path(f"{settings.MEDIA_ROOT}/{output_folder}_{tag_ver}")
         image_outs = outs / "images"
         label_outs = outs / "labels"
         image_outs.mkdir(parents=True, exist_ok=True)
@@ -145,14 +144,14 @@ def process_augmentation(request):
                 
                 for image_filename, image, label_filename, bboxes in dataset:
 
-                    label_filename = f"{label_filename[:-4]}_{fn_suffix}_{filename_extension}.txt"
+                    label_filename = f"{label_filename[:-4]}_{tag_ver}_{filename_extension}.txt"
                     saved_label = label_outs / label_filename
 
                     with open(saved_label, 'a') as label_file:
                         for bbox in bboxes:
                             label_file.write(f"{str(bbox[-1])} {str(bbox[0])} {str(bbox[1])} {str(bbox[2])} {str(bbox[3])}\n")
 
-                    image_filename = f"{image_filename[:-4]}_{fn_suffix}_{filename_extension}.png"
+                    image_filename = f"{image_filename[:-4]}_{tag_ver}_{filename_extension}.png"
                     saved_image = image_outs / image_filename
                     image = Image.fromarray(image)
                     image.save(saved_image)
@@ -178,14 +177,14 @@ def process_augmentation(request):
             )
 
             for image_filename, image, label_filename, bboxes in dataset:
-                label_filename = f"{label_filename[:-4]}_{fn_suffix}_{filename_extension}.txt"
+                label_filename = f"{label_filename[:-4]}_{tag_ver}_{filename_extension}.txt"
                 saved_label = label_outs / label_filename
 
                 with open(saved_label, 'a') as label_file:
                     for bbox in bboxes:
                         label_file.write(f"{str(bbox[-1])} {str(bbox[0])} {str(bbox[1])} {str(bbox[2])} {str(bbox[3])}\n")
 
-                image_filename = f"{image_filename[:-4]}_{fn_suffix}_{filename_extension}.png"
+                image_filename = f"{image_filename[:-4]}_{tag_ver}_{filename_extension}.png"
                 saved_image = image_outs / image_filename
                 image = Image.fromarray(image)
                 image.save(saved_image)
@@ -195,8 +194,6 @@ def process_augmentation(request):
     else:
         return HttpResponseRedirect('index')
     
-
-
 
 # helper function
 def text_to_hex(text):
